d12c5b38407a6999a974bf1ed6c7c53142dc4148
apmarshall <apmarshall@soren.tech>
2018-03-14 11:04:38 -0400

    Update to comments template based on 10up twentysixteen react and the Foxhound theme

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,27 +1,25 @@
 import React, {Component} from React;
 import "./Comments.css";
 
-class Comments extends Component {
+class Comments extends React.Component {
     render() {
-        <div id="comments">
-            <h3 id="comments-title">{this.props.commentsNumber} Responses to {this.props.postTitle}</h3>
-            // Page Navigation - import from another component ??
-            <ul>
-                <li class={this.props.commentClass} id="li-comment-{this.props.commentID}">
-                    //Vcard component -- <Vcard avatar={this.props.comment} />
-                    if (this.props.commentApproved == 0) {
-                        <em class="comment-awaiting-moderation">Your comment is awaiting moderation</em>
-                    } else {
-                        <div class="commentMeta commentMetaData">
-                            {this.props.commentDate}
-                            {this.props.commentTime}
-                            {this.props.commentEditLink}
-                        </div>
-                        <div class="commentBody">
-                            {this.props.commentText}
-                        </div>
-                        <ReplyLink /> // additional component
-             </ul>
-         </div><!-- comments -->
+        if (this.props.protected) {
+            return(
+                <div id="comments">
+                    <p className="nopassword">This post is password protected. Enter the password to view any comments.</p>
+                </div><!-- #comments -->
+            )
+        }
+        let titleString = 'One Response to ';
+        if (this.props.post.commentsNumber > 1) {
+            titleString = '${this.props.post.commentsNumber} Responses to ';
+        }
+        return(
+            <div id="comments">
+                <h3 id="comments-title">{titleString}"${this.props.post.title}"</h3>
+                <ol className="commentlist">
+                    {this.props.post.comments}
+                </ol>
+            </div><!-- #comments -->
+        )
      }
-
